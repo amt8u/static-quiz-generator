a486ac4112d72c4b196e877066bcd2b1b004ffeb
Amit Upadhyay <amupadhyay@qualys.com>
2020-11-10 01:05:28 +0530

    html generation building with json

--- index.js
+++ index.js
@@ -1,32 +1,21 @@
 var fs = require('fs');
+var path = require('path');
 var Handlebars = require("handlebars");
 
-// var fileName = 'output.html';
-// var stream = fs.createWriteStream(fileName);
-
-// stream.once('open', function(fd) {
-//   console.log("argv0", process.argv[0]);
-//   console.log("argv1", process.argv[1]);
-//   console.log("argv2", process.argv[2]);
-//   console.log("argv3", process.argv[3]);
-//   var questionsJson = fd.readFile(process.argv[2]);
-//   var html = buildHtml(questionsJson);
-
-//   stream.end(html);
-// });
-
-  console.log("argv0", process.argv[0]);
-  console.log("argv1", process.argv[1]);
-  console.log("argv2", process.argv[2]);
-  console.log("argv3", process.argv[3]);
+console.log("Reading json data from ", process.argv[2]);
 
 var questionsJson = fs.readFileSync(process.argv[2], "utf-8");
+// questionsJson is a buffer
 var html = buildHtml(JSON.parse(questionsJson));
 
 function buildHtml(fileData) {
-  var template = Handlebars.compile("{{name}}");
-  console.log(fileData);
-  var html = template(fileData);
-  fs.writeFileSync("output.html", html);
-  console.log("Success");
+  var template = fs.readFileSync(path.resolve(path.join(__dirname, 'template.hbs')), "utf-8");
+  var renderTemplate = Handlebars.compile(template);
+  // console.log(fileData);
+  var html = renderTemplate(fileData);
+  // Write to build folder. Copy the built file and deploy
+  fs.writeFile("./build/index.html", html, err => {
+    if (err) console.log(err);
+    console.log("File written succesfully");
+  });
 }
